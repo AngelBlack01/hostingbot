1d73e859af9306738c7494d39bdfa13eb6260828
SrDaniez <109402760+SrDaniez@users.noreply.github.com>
2022-09-26 16:53:15 -0500

    Create quetangayeres.js

---
+++ comandos/🔰 Info/quetangayeres.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const { MessageActionRow, MessageSelectMenu, MessageEmbed, MessageButton} = require('discord.js')
+const { EmbedBuilder } = require("discord.js")
+
+module.exports = {
+  name: "quetangayeres",
+  aliases: ["gay", "quegay", "cuangayeres", "sergay"],
+  desc: "Sirve Para Ver Cuanto De Gay Eres.",
+async run(client, message, args) {
+
+    let respuestas = [Math.floor(Math.random() * 100)]
+    let random = respuestas[Math.floor(Math.random() * respuestas.length)];
+    let dinero = [Math.floor(Math.random() * 100)]
+    let dn = dinero[Math.floor(Math.random() * respuestas.length)];
+    let salud = [Math.floor(Math.random() * 100)]
+    let sl = salud[Math.floor(Math.random() * respuestas.length)];
+    let suerte = [Math.floor(Math.random() * 100)]
+    let sr = suerte[Math.floor(Math.random() * respuestas.length)];
+    const embed = new Discord.EmbedBuilder()
+    .setColor(0x00ffeb)
+    .setTitle('Que Tan Gay Eres?')
+    .addFields(
+      { name: '**🏳️‍🌈 Tu Porcentaje De Gay Es:**', value: `**<a:_:1016057216362618921> ${random}%  De Gay**` },
+    )
+    .setTimestamp()
+    .setFooter({ text: 'MrDxniezzz © 2022', iconURL: 'https://cdn.discordapp.com/attachments/1017639960502874132/1018583114584445110/Darkhush.png' });
+
+  message.channel.send({ embeds: [embed] });
+
+  }
+
+} 
